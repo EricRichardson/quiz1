2bba887dc1e24a3b71457f3cfd36e888fbb11837
Eric Richardson <eric@ericonrails.com>
2016-05-30 11:57:26 -0700

    Edited explanation

--- bonus.rb
+++ bonus.rb
@@ -1,6 +1,4 @@
-# To write a method that doesn't mutate variables just make new variable to hold onto new objects. This is safer since you won't accidently modify a variable being used elsewhere.
-
-# You can use a regex for also. I got the part \b(?<!['â`])[a-z] from http://apidock.com/rails/ActiveSupport/Inflector/titleize I then added a negative look ahead to ignore the words we want to leave downcase. This solution should run faster than my old one, albeit harder to read. I then upcase the first letter and add the rest of the string to ensure the first letter is capitalized no matter what. If we did not want this w could take out this line.
+# You can use a regex. I got the part \b(?<!['â`])[a-z] from http://apidock.com/rails/ActiveSupport/Inflector/titleize I then added a negative look ahead to ignore the words we want to leave downcase. This solution should run faster than my old one, albeit harder to read. I then upcase the first letter and add the rest of the string to ensure the first letter is capitalized no matter what. If we did not want this w could take out this line.
 
 def titilize(string)
 s = string.gsub(/(?!or|from|to|and|in|the|of)\b(?<!['])[a-z]/) { $&.capitalize }
